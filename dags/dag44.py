6a6ff6195d8984755d03e715d3ae09be5d489c6f
Geethanjali0225 <geetanjali.c2k@gmail.com>
2024-01-29 12:54:52 +0530

    modified dag44

--- dags/dag44.py
+++ dags/dag44.py
@@ -14,7 +14,7 @@ default_args = {
 
 # Instantiate the DAG
 dag = DAG(
-    'hellodag',
+    'hello_dag',
     default_args=default_args,
     description='Example DAG',
     schedule_interval=timedelta(days=1),
