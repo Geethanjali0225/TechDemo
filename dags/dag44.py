ae5b66f65626ec1204eeca528a5906884251556b
Geethanjali0225 <geetanjali.c2k@gmail.com>
2024-01-29 11:42:50 +0530

    updated dags

--- dags/dag44.py
+++ dags/dag44.py
@@ -6,7 +6,7 @@ from airflow.operators.python_operator import PythonOperator
 # Define default_args dictionary to specify default parameters for the DAG
 default_args = {
     'owner': 'geethanjali',
-    'start_date': datetime(2024, 1, 1),
+    'start_date': datetime.utcnow(),
     'depends_on_past': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
@@ -14,17 +14,17 @@ default_args = {
 
 # Instantiate the DAG
 dag = DAG(
-    'tutorial_dag123',
+    'tutorial',
     default_args=default_args,
     description='A simple example DAG',
-    schedule_interval=timedelta(days=1),  # Run the DAG daily
+    schedule_interval=timedelta(days=1),
 )
 
-# Define tasks in the DAG
+# tasks
 start_task = DummyOperator(task_id='start_task', dag=dag)
 
 def print_hello():
-    print("Hello, this is a Python Operator task!")
+    print("Hello")
 
 python_task = PythonOperator(
     task_id='python_task',
