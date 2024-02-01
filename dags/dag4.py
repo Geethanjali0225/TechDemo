8e7fa243a21549d3c24ea539201d5822bbc2ae90
Geethanjali0225 <geetanjali.c2k@gmail.com>
2024-02-01 11:38:46 +0530

    added dag4

---
+++ dags/dag4.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime.utcnow(),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'demo_dag',
+    default_args=default_args,
+    description='A simple example DAG',
+    schedule_interval='@daily',
+)
+
+task1 = BashOperator(
+    task_id='task_1',
+    bash_command='echo "Hello"',
+    dag=dag,
+)
+
+task2 = BashOperator(
+    task_id='task_2',
+    bash_command='echo "World"',
+    dag=dag,
+)
+
+task1 >> task2
